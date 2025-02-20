156168e8c50a10fb4291160dc6cf23e6b87793e1
ericksh2208 <soto.erick.2024@cbtis-246.edu.mx>
2025-02-20 14:57:37 -0600

    Add files via upload

---
+++ practica 2.py
@@ -0,0 +1,24 @@
+#Matería:CodSofSisln.
+#Fecha:20/02/25|Grupo 2° "J".
+#Alumno:Erick Otoniel Soto Hdez.
+#Nombre de la practica:Operaciones con cadenas.
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+nombre = input("cual es tu nombre")
+print("hola ", nombre)
+apellido = input("cual es tu apellido")
+print("hola ",nombre,' ', apellido)
+print("tu nombre invertido es ",nombre[::-1])
+print("tu apellido invertido es ",apellido[::-1])
+
+
+# In[ ]:
+
+
+
+
